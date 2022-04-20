d1fb773f4e276f408a52e740cf1fcb96699a5e25
Pliszka <karoliniakkjk@gmail.com>
2022-04-20 22:48:45 +0200

    INFO page about successfully sending message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 NPOINT = 'https://api.npoint.io/772691b33e267dc84ebd'
@@ -23,6 +23,16 @@ def contact():
 @app.route('/article/<article_id>')
 def article_page(article_id):
     return render_template("post.html", article=articles[int(article_id)-1])
+
+@app.route('/form-entry', methods=['POST', 'GET'])
+def recive():
+    data = request.form
+    print(data["your name"])
+    print(data["your email"])
+    print(data["your phone"])
+    print(data["your message"])
+    return "<h1>Successfully sent your message</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
